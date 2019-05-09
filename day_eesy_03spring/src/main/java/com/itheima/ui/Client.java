6a01db391dfdaf41c08f17431d67ff3990c5608d
stay9611 <stay9611@foxmail.com>
2019-05-09 19:53:53 +0800

    08.添加spring工程

---
+++ day_eesy_03spring/src/main/java/com/itheima/ui/Client.java
@@ -0,0 +1,29 @@
+package com.itheima.ui;
+
+import com.itheima.dao.IAccountDao;
+import com.itheima.service.IAccountService;
+import com.itheima.service.impl.AccountServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * 模拟一个表现层，用于调用业务层
+ */
+public class Client {
+    /**
+     * 获取spring的ioc核心容器，并根据id获取对象
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+        //获取核心容器对象
+        ApplicationContext ac = new ClassPathXmlApplicationContext("bean.xml");
+        IAccountService as = (IAccountService) ac.getBean("accountService");
+        IAccountDao adao = ac.getBean("accountDao", IAccountDao.class);
+        System.out.println(as);
+        System.out.println(adao);
+        //   as.saveAccount();
+    }
+
+}
+
